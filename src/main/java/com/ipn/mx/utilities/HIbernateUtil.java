f2203db9692adc8b0e698e60e631815eabb7bf73
zanzakigus <erz_ra@hotmail.com>
2021-12-09 14:48:52 -0600

    commit inicial

---
+++ src/main/java/com/ipn/mx/utilities/HIbernateUtil.java
@@ -0,0 +1,36 @@
+package com.ipn.mx.utilities;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HIbernateUtil {
+
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory(){
+        if(sessionFactory==null){
+            try {
+                registry= new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+                MetadataSources mds = new MetadataSources(registry);
+                Metadata metadata = mds.getMetadataBuilder().build();
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            }catch (Exception e){
+                if(registry!=null){
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+
+        return sessionFactory;
+    }
+
+    public static void shotdown(){
+        if(registry!=null){
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
