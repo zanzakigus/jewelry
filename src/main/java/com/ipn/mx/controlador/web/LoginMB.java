c8c687e7fa0e09055a05b222151dd243035465db
zanzakigus <erz_ra@hotmail.com>
2021-12-30 23:01:53 -0600

    login

--- src/main/java/com/ipn/mx/controlador/web/LoginMB.java
+++ src/main/java/com/ipn/mx/controlador/web/LoginMB.java
@@ -1,31 +1,31 @@
 package com.ipn.mx.controlador.web;
 
 
-
 import com.ipn.mx.dao.UsuarioDAO;
+import com.ipn.mx.entities.Producto;
 import com.ipn.mx.entities.Usuario;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
 import java.io.Serializable;
 
 /**
- *
  * @author zanzakigus
  */
 @ManagedBean(name = "loginMB")
-@SessionScoped
-public class LoginMB extends BaseBean implements Serializable{
+@RequestScoped
+public class LoginMB extends BaseBean implements Serializable {
 
 
     private final UsuarioDAO dao = new UsuarioDAO();
 
     private Usuario dto;
-    private String respuesta=null;
+    private boolean respuesta;
+    private boolean logged;
 
     /**
      * Creates a new instance of IndexMB
@@ -41,41 +41,59 @@ public class LoginMB extends BaseBean implements Serializable{
         this.dto = dto;
     }
 
-    public String getRespuesta() {
+    public boolean getRespuesta() {
         return respuesta;
     }
 
-    public void setRespuesta(String respuesta) {
+    public void setRespuesta(boolean respuesta) {
         this.respuesta = respuesta;
     }
+
+    public void setLogged(boolean logged) {
+        this.logged = logged;
+    }
+
+    public boolean getLogged() {
+        return logged;
+    }
+
+
+
     @PostConstruct
     public void init() {
         this.dto = new Usuario();
+
     }
 
     public String preparedIndex() {
         init();
         return "/login?faces-redirect=true";
     }
-
-    public void login(){
+    public String preparedLogged() {
+        return "/index.xhtml?faces-redirect=true";
+    }
+    public void login() {
 /*        String email = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("txtUsuarioPassword");
         String password = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("claveSel");*/
-        setRespuesta(null);
         try {
             if ((this.dto = dao.login(dto)) != null) {
                 FacesContext facesContext = FacesContext.getCurrentInstance();
                 HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
                 session.setAttribute("usuario", dto.getNombreUsuario());
                 session.setAttribute("tipo", dto.getIdTipoRol().getDescripcionTipoRol());
+                logged = true;
+                preparedLogged();
             } else {
                 FacesContext facesContext = FacesContext.getCurrentInstance();
                 HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
                 if (session != null) {
                     session.invalidate();
                 }
-                setRespuesta("Credenciales invalidas");
+                System.out.println("so esyou aqui");
+                respuesta = true;
+                logged = false;
                 preparedIndex();
+
             }
 
         } catch (Exception e) {
