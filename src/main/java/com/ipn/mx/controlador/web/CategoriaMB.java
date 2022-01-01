a8e61059214ba70aa02e0f63d88f96664b25b3ce
zanzakigus <erz_ra@hotmail.com>
2022-01-01 14:28:14 -0600

    categoria grafica y reporte

--- src/main/java/com/ipn/mx/controlador/web/CategoriaMB.java
+++ src/main/java/com/ipn/mx/controlador/web/CategoriaMB.java
@@ -5,9 +5,13 @@ import com.ipn.mx.dao.ProductoDAO;
 import com.ipn.mx.entities.Categoria;
 import com.ipn.mx.entities.Grafica;
 import com.ipn.mx.entities.Producto;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperRunManager;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.internal.SessionImpl;
 import org.hibernate.query.Query;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
@@ -18,14 +22,21 @@ import org.jfree.data.general.PieDataset;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -189,6 +200,42 @@ public class CategoriaMB extends BaseBean implements Serializable {
             dpd.setValue(dto.getNombre(), dto.getCantidad());
         }
         return dpd;
+
+    }
+
+    public void mostrarReporte() {
+
+
+        try {
+            Session s  = (new Configuration().configure().buildSessionFactory()).openSession();
+            SessionImpl sessionImpl = (SessionImpl) s;
+            Connection conn = sessionImpl.connection();
+            ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+
+            FacesContext fc = FacesContext.getCurrentInstance();
+            ExternalContext ec = fc.getExternalContext();
+            File report =  new File(servletContext.getRealPath("/reports/report.jasper"));
+            byte[] b = JasperRunManager.runReportToPdf(report.getPath(),null, conn);
+            ec.responseReset();
+            ec.setResponseContentType("application/pdf");
+            ec.setResponseContentLength((int)b.length);
+            String repor ="reportw";
+
+            //Inline
+            //ec.setResponseHeader("Content-Disposition", "inline; filename=\"" + repor + "\"");
+
+            //Attach for Browser
+            //ec.setResponseHeader("Content-Disposition", "attachment; filename=\"" + report.getName() + "\"");
+
+            OutputStream sos = ec.getResponseOutputStream();
+            sos.write(b,0, b.length);
+            sos.flush();
+            sos.close();
+            fc.responseComplete();
+        } catch (IOException | JRException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
